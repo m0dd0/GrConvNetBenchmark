d8a3dc195bc3f49ef3a2f85c9c72bf8460759eca
Moritz Hesche <mo.hesche@gmail.com>
2023-02-14 11:02:25 +0100

    finalizing config utility

--- grconvnet/utils/processing.py
+++ grconvnet/utils/processing.py
@@ -2,10 +2,6 @@
 """
 
 from typing import Dict, Any, Callable
-from copy import deepcopy
-from pathlib import Path
-import importlib
-import yaml
 
 import torch
 
@@ -13,31 +9,6 @@ from grconvnet.datatypes import CameraData
 from grconvnet.preprocessing import Preprocessor, PreprocessorBase
 from grconvnet.postprocessing import Postprocessor, PostprocessorBase
 from grconvnet.models import GenerativeResnet
-
-
-def module_from_config(config: Dict[str, Any]):
-    config = deepcopy(config)
-
-    import_path = config.pop("class").split(".")
-    module_cls = getattr(
-        importlib.import_module(".".join(import_path[:-1])), import_path[-1]
-    )
-
-    constructor_name = config.pop("constructor", None)
-    if constructor_name is not None:
-        module_cls = getattr(module_cls, constructor_name)
-
-    module_kwargs = {}
-
-    for arg_name, arg_value in config.items():
-        if isinstance(arg_value, dict) and "class" in arg_value:
-            submodule_config = arg_value
-            submodule = module_from_config(submodule_config)
-            module_kwargs[arg_name] = submodule
-        else:
-            module_kwargs[arg_name] = arg_value
-
-    return module_cls(**module_kwargs)
 
 
 class End2EndProcessor:
@@ -50,7 +21,7 @@ class End2EndProcessor:
     ):
         # TODO use or syntax for default values
         if model is None:
-            model = GenerativeResnet.from_state_dict_path()
+            model = GenerativeResnet.from_jit()
         self.model = model
 
         if preprocessor is None:
