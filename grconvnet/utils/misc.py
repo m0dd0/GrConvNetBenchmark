64d99081bfbcadb063c5b95bda70ac3f83d2d710
Moritz Hesche <mo.hesche@gmail.com>
2023-02-22 15:44:13 +0100

    cleaned utils

--- grconvnet/utils/misc.py
+++ grconvnet/utils/misc.py
@@ -6,69 +6,6 @@ import torch
 
 from grconvnet.models.grconvnet import GenerativeResnet
 
-MODELS_PATH = Path(__file__).parent.parent / "trained-models-jit"
-
-
-def get_model_path(model_name: str, version_name: str):
-    return MODELS_PATH / model_name / version_name
-
-
-def list_pretrained_models() -> Dict[str, List[str]]:
-    """Get an overview about all exisitng pretrained models.
-    The result is an Dictionairy where all keys are the name of the model/the corresponding
-    folder and the keys are a list of file names which give the name of the model
-    versions containes in the model folder.
-
-    Returns:
-        Dict[str, List[str]]: All models and their versions.
-    """
-    models = {
-        model_dir.name: [f.name for f in model_dir.iterdir() if f.suffix == ".pt"]
-        for model_dir in MODELS_PATH.iterdir()
-        if model_dir.is_dir()
-    }
-
-    return models
-
-
-def load_pretrained(
-    name: str = "cornell-randsplit-rgbd-grconvnet3-drop1-ch32",
-    version: str = None,
-) -> torch.nn.Module:
-    """Loads one of the pretrained models as a pytorch Model class.
-    This uses the jit loading option so the model can be used independently of the
-    repository structure. However a grconvnet3 class is instantiated afterwards
-    and setup with the weidgts from the loaded network in order to make the
-    network runnable.
-
-    Args:
-        name (str, optional): The name od the model to load. This must be a name
-            of a subfolder in the 'trained-models' folder (= a kay from the dict
-            obtained by list_pretrained_models()).
-            Defaults to "cornell-randsplit-rgbd-grconvnet3-drop1-ch32".
-        version (str, optional): The name of the version of the model.
-            This must be a name of a model file contained in the model subfolder.
-            Defaults to the first one in the folder sorted alphabetically.
-
-    Returns:
-        torch.nn.Module: The loaded pytorch module.
-    """
-    model_dir = MODELS_PATH / name
-
-    if version is None:
-        versions = [f.name for f in model_dir.iterdir() if f.suffix == ".pt"]
-        version = sorted(versions)[0]
-
-    model_path = model_dir / version
-
-    model = torch.jit.load(model_path)
-
-    new_model = GenerativeResnet()
-    new_model.load_state_dict(model.state_dict())
-    model = new_model
-
-    return model
-
 
 def convert_models():
     """An added utility script which tries to load the pickled pretrained models and
--- grconvnet/utils/processing.py
+++ grconvnet/utils/processing.py
@@ -1,246 +1,132 @@
-"""_summary_
-"""
+# TODO rewrite
+# def process_dataset(
+#     dataset,
+#     e2e_processor: End2EndProcessor,
+#     exporter: Exporter,
+#     batch_size=10,
+# ):
+#     for i_batch in range((len(dataset) // batch_size) + 1):
+#         j_start = i_batch * batch_size
+#         j_end = min((i_batch + 1) * batch_size, len(dataset))
+#         batch = [dataset[j] for j in range(j_start, j_end)]
+#         print(f"Processing samples {j_start}...{j_end-1}")
 
-from typing import Dict, Any, Callable, List
-from pathlib import Path
-from copy import deepcopy
+#         process_data_batch = e2e_processor(batch)
 
-import matplotlib.pyplot as plt
-import torch
-import yaml
+#         for process_data in process_data_batch:
+#             fig = vis.overview_fig(
+#                 fig=plt.figure(figsize=(20, 20)),
+#                 original_rgb=vis.make_tensor_displayable(
+#                     process_data["sample"].rgb, True, True
+#                 ),
+#                 preprocessed_rgb=vis.make_tensor_displayable(
+#                     process_data["preprocessor"]["rgb_masked"], True, True
+#                 ),
+#                 q_img=vis.make_tensor_displayable(
+#                     process_data["postprocessor"]["q_img"], False, False
+#                 ),
+#                 angle_img=vis.make_tensor_displayable(
+#                     process_data["postprocessor"]["angle_img"], False, False
+#                 ),
+#                 width_img=vis.make_tensor_displayable(
+#                     process_data["postprocessor"]["width_img"], False, False
+#                 ),
+#                 image_grasps=process_data["grasps_img"],
+#                 world_grasps=process_data["grasps_world"]
+#                 if process_data["sample"].cam_intrinsics is not None
+#                 else None,
+#                 cam_intrinsics=process_data["sample"].cam_intrinsics,
+#                 cam_rot=process_data["sample"].cam_rot,
+#                 cam_pos=process_data["sample"].cam_pos,
+#             )
+#             plt.close(fig)
 
-from grconvnet.dataloading.datasets import CornellDataset, YCBSimulationData
-from grconvnet.datatypes import CameraData
-from grconvnet.preprocessing import Preprocessor, PreprocessorBase
-from grconvnet.postprocessing import (
-    Postprocessor,
-    PostprocessorBase,
-    Img2WorldCoordConverter,
-    Decropper,
-)
-from grconvnet.models import GenerativeResnet
-from grconvnet.utils import visualization as vis
-from grconvnet.utils.export import Exporter
-from grconvnet.utils.config import module_from_config
+#             export_data = {
+#                 "original_rgb": process_data["sample"].rgb,
+#                 "rgb_cropped": process_data["preprocessor"]["rgb_cropped"],
+#                 "depth_cropped": process_data["preprocessor"]["depth_cropped"],
+#                 "rgb_masked": process_data["preprocessor"]["rgb_masked"],
+#                 "q_img": process_data["postprocessor"]["q_img"],
+#                 "angle_img": process_data["postprocessor"]["angle_img"],
+#                 "width_img": process_data["postprocessor"]["width_img"],
+#                 "grasps_img": process_data["grasps_img"],
+#                 "grasps_world": process_data["grasps_world"]
+#                 if process_data["sample"].cam_intrinsics is not None
+#                 else None,
+#                 "cam_intrinsics": process_data["sample"].cam_intrinsics
+#                 if process_data["sample"].cam_intrinsics is not None
+#                 else None,
+#                 "cam_pos": process_data["sample"].cam_pos
+#                 if process_data["sample"].cam_pos is not None
+#                 else None,
+#                 "cam_rot": process_data["sample"].cam_rot
+#                 if process_data["sample"].cam_rot is not None
+#                 else None,
+#                 "model_input": process_data["model_input"],
+#                 "overview": fig,
+#             }
 
-
-class End2EndProcessor:
-    def __init__(
-        self,
-        model: GenerativeResnet = None,
-        preprocessor: PreprocessorBase = None,
-        postprocessor: PostprocessorBase = None,
-        img2world_converter: Callable = None,
-    ):
-        """This is a utility class that combines the preprocessing, inference and
-        postprocessing steps into a single callable object. While executing the
-        different steps, it also collects intermediate results and stores them.
-        These results can be used for debugging or visualization purposes.
-        The input of the call is a list of samples, which are processed in a
-        batched manner. The output is a list of dictionaries, where each dictionary
-        contains the results of the processing steps for a single sample.
-        Note that this used no dataloader to since the dataloader is not
-        able to output the intermediate results in the preprocessing steps.
-        Therfore this module only is for evaluation and debugging purposes and not
-        for training.
-
-        Args:
-            model (GenerativeResnet, optional): _description_. Defaults to None.
-            preprocessor (PreprocessorBase, optional): _description_. Defaults to None.
-            postprocessor (PostprocessorBase, optional): _description_. Defaults to None.
-            img2world_converter (Callable, optional): _description_. Defaults to None.
-        """
-        self.model = model or GenerativeResnet.from_jit()
-        self.preprocessor = preprocessor or Preprocessor()
-        self.postprocessor = postprocessor or Postprocessor()
-        self.img2world_converter = img2world_converter
-
-    def __call__(self, samples: List[CameraData]) -> List[Dict[str, Any]]:
-        # batched preprocessing
-        input_batch = []
-        preprocessor_results = []
-        for sample in samples:
-            input_batch.append(self.preprocessor(sample))
-            preprocessor_results.append(
-                deepcopy(self.preprocessor.intermediate_results)
-            )
-        input_batch = torch.stack(input_batch)
-
-        # batched inference
-        with torch.no_grad():
-            predictions = self.model(input_batch)
-
-        # batched postprocessing
-        grasps_img_batch = []
-        posprocessor_results = []
-        for output in predictions:
-            grasps_img_batch.append(self.postprocessor(output))
-            posprocessor_results.append(
-                deepcopy(self.postprocessor.intermediate_results)
-            )
-
-        # batched img2world conversion
-        grasps_world_batch = []
-        # img2world_converter_results = [] # TODO support this
-        if self.img2world_converter is not None:
-            for i_sample in range(len(samples)):
-                grasps_img = grasps_img_batch[i_sample]
-                sample = samples[i_sample]
-                grasps_world = [
-                    self.img2world_converter(g_img, sample.depth)
-                    for g_img in grasps_img
-                ]
-                grasps_world_batch.append(grasps_world)
-
-        # batched data collection
-        process_data_batch = []
-        for i_sample in range(len(samples)):
-            process_data = {
-                "preprocessor": preprocessor_results[i_sample],
-                "postprocessor": posprocessor_results[i_sample],
-                # "img2world_converter": img2world_converter_results[i_sample]
-                # if self.img2world_converter is not None
-                # else None,
-                "model_input": input_batch[i_sample],
-                "sample": samples[i_sample],
-                "grasps_img": grasps_img_batch[i_sample],
-                "grasps_world": grasps_world_batch[i_sample]
-                if self.img2world_converter is not None
-                else None,
-            }
-            process_data_batch.append(process_data)
-
-        return process_data_batch
+#             _ = exporter(export_data, f"{process_data['sample'].name}")
 
 
-def process_dataset(
-    dataset,
-    e2e_processor: End2EndProcessor,
-    exporter: Exporter,
-    batch_size=10,
-):
-    for i_batch in range((len(dataset) // batch_size) + 1):
-        j_start = i_batch * batch_size
-        j_end = min((i_batch + 1) * batch_size, len(dataset))
-        batch = [dataset[j] for j in range(j_start, j_end)]
-        print(f"Processing samples {j_start}...{j_end-1}")
+# def process_cornell(dataset_path, config_path, export_path, batch_size):
+#     with open(config_path) as f:
+#         config = yaml.safe_load(f)
 
-        process_data_batch = e2e_processor(batch)
+#     dataset = CornellDataset(dataset_path)
 
-        for process_data in process_data_batch:
-            fig = vis.overview_fig(
-                fig=plt.figure(figsize=(20, 20)),
-                original_rgb=vis.make_tensor_displayable(
-                    process_data["sample"].rgb, True, True
-                ),
-                preprocessed_rgb=vis.make_tensor_displayable(
-                    process_data["preprocessor"]["rgb_masked"], True, True
-                ),
-                q_img=vis.make_tensor_displayable(
-                    process_data["postprocessor"]["q_img"], False, False
-                ),
-                angle_img=vis.make_tensor_displayable(
-                    process_data["postprocessor"]["angle_img"], False, False
-                ),
-                width_img=vis.make_tensor_displayable(
-                    process_data["postprocessor"]["width_img"], False, False
-                ),
-                image_grasps=process_data["grasps_img"],
-                world_grasps=process_data["grasps_world"]
-                if process_data["sample"].cam_intrinsics is not None
-                else None,
-                cam_intrinsics=process_data["sample"].cam_intrinsics,
-                cam_rot=process_data["sample"].cam_rot,
-                cam_pos=process_data["sample"].cam_pos,
-            )
-            plt.close(fig)
+#     e2e_processor = module_from_config(config)
 
-            export_data = {
-                "original_rgb": process_data["sample"].rgb,
-                "rgb_cropped": process_data["preprocessor"]["rgb_cropped"],
-                "depth_cropped": process_data["preprocessor"]["depth_cropped"],
-                "rgb_masked": process_data["preprocessor"]["rgb_masked"],
-                "q_img": process_data["postprocessor"]["q_img"],
-                "angle_img": process_data["postprocessor"]["angle_img"],
-                "width_img": process_data["postprocessor"]["width_img"],
-                "grasps_img": process_data["grasps_img"],
-                "grasps_world": process_data["grasps_world"]
-                if process_data["sample"].cam_intrinsics is not None
-                else None,
-                "cam_intrinsics": process_data["sample"].cam_intrinsics
-                if process_data["sample"].cam_intrinsics is not None
-                else None,
-                "cam_pos": process_data["sample"].cam_pos
-                if process_data["sample"].cam_pos is not None
-                else None,
-                "cam_rot": process_data["sample"].cam_rot
-                if process_data["sample"].cam_rot is not None
-                else None,
-                "model_input": process_data["model_input"],
-                "overview": fig,
-            }
+#     exporter = Exporter(export_dir=export_path)
 
-            _ = exporter(export_data, f"{process_data['sample'].name}")
+#     export_path.mkdir(parents=True, exist_ok=True)
+#     with open(export_path / "inference_config.yaml", "w") as f:
+#         yaml.dump(config, f)
+
+#     process_dataset(dataset, e2e_processor, exporter, batch_size)
 
 
-def process_cornell(dataset_path, config_path, export_path, batch_size):
-    with open(config_path) as f:
-        config = yaml.safe_load(f)
+# def process_ycb(dataset_path, config_path, export_path, batch_size):
+#     # load config
+#     with open(config_path) as f:
+#         config = yaml.safe_load(f)
 
-    dataset = CornellDataset(dataset_path)
+#     dataset = YCBSimulationData(dataset_path)
 
-    e2e_processor = module_from_config(config)
+#     # instantiate e2e processor
+#     sample = dataset[0]
+#     e2e_processor = module_from_config(config)
+#     e2e_processor.img2world_converter.coord_converter = Img2WorldCoordConverter(
+#         sample.cam_intrinsics, sample.cam_rot, sample.cam_pos
+#     )
+#     e2e_processor.img2world_converter.decropper = Decropper(
+#         resized_in_preprocess=config["preprocessor"]["resize"],
+#         original_img_size=sample.rgb.shape[1:],
+#     )
 
-    exporter = Exporter(export_dir=export_path)
+#     exporter = Exporter(export_dir=export_path)
 
-    export_path.mkdir(parents=True, exist_ok=True)
-    with open(export_path / "inference_config.yaml", "w") as f:
-        yaml.dump(config, f)
+#     # save config
+#     export_path.mkdir(parents=True, exist_ok=True)
+#     with open(export_path / "inference_config.yaml", "w") as f:
+#         yaml.dump(config, f)
 
-    process_dataset(dataset, e2e_processor, exporter, batch_size)
-
-
-def process_ycb(dataset_path, config_path, export_path, batch_size):
-    # load config
-    with open(config_path) as f:
-        config = yaml.safe_load(f)
-
-    dataset = YCBSimulationData(dataset_path)
-
-    # instantiate e2e processor
-    sample = dataset[0]
-    e2e_processor = module_from_config(config)
-    e2e_processor.img2world_converter.coord_converter = Img2WorldCoordConverter(
-        sample.cam_intrinsics, sample.cam_rot, sample.cam_pos
-    )
-    e2e_processor.img2world_converter.decropper = Decropper(
-        resized_in_preprocess=config["preprocessor"]["resize"],
-        original_img_size=sample.rgb.shape[1:],
-    )
-
-    exporter = Exporter(export_dir=export_path)
-
-    # save config
-    export_path.mkdir(parents=True, exist_ok=True)
-    with open(export_path / "inference_config.yaml", "w") as f:
-        yaml.dump(config, f)
-
-    process_dataset(dataset, e2e_processor, exporter, batch_size)
+#     process_dataset(dataset, e2e_processor, exporter, batch_size)
 
 
-if __name__ == "__main__":
-    batch_size = 10
-    # config_suffix = "_no_pos_mask"
-    config_suffix = ""
-    i = 2
+# if __name__ == "__main__":
+#     batch_size = 10
+#     config_suffix = "_no_pos_mask"
+#     # config_suffix = ""
+#     i = 3
 
-    process_ycb(
-        dataset_path=Path.home() / "Documents" / f"ycb_sim_data_{i}",
-        config_path=Path(__file__).parent.parent
-        / "configs"
-        / f"ycb_inference{config_suffix}.yaml",
-        export_path=Path(__file__).parent.parent
-        / "results"
-        / f"ycb_{i}_b{batch_size}{config_suffix}",
-        batch_size=batch_size,
-    )
+#     process_ycb(
+#         dataset_path=Path.home() / "Documents" / f"ycb_sim_data_{i}",
+#         config_path=Path(__file__).parent.parent
+#         / "configs"
+#         / f"ycb_inference{config_suffix}.yaml",
+#         export_path=Path(__file__).parent.parent
+#         / "results"
+#         / f"ycb_{i}_b{batch_size}{config_suffix}",
+#         batch_size=batch_size,
+#     )
